b641165efff5c17b8c8f83fc9d836508aa0e4270
Sakshi Waitage <sakshiwaitage245@gmail.com>
2025-11-23 13:05:16 +0530

    Add welcom message

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -106,7 +106,7 @@ import { all } from "axios";
       <div className="max-w-7xl mx-auto px-4 py-8">
   
         <p className="text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
-          Welcome, Joe Doe
+          Welcome, {user?.name}
         </p>
   
         {/* CREATE & UPLOAD */}
